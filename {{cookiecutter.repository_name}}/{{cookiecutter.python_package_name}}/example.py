6480a37921e62dc8d385fb361fad5a750fd1597c
rmorshea <ryan.morshead@gmail.com>
2021-01-08 15:52:56 -0800

    Just use dot-notation in idom.Module

--- {{cookiecutter.repository_name}}/{{cookiecutter.python_package_name}}/example.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.python_package_name}}/example.py
@@ -2,15 +2,15 @@ from pathlib import Path
 
 import idom
 
+
 _js_module = idom.Module(
     "{{ cookiecutter.npm_package_name }}",
     source_file=Path(__file__).parent / "bundle.js"
 )
-_ExampleComponent = _js_module.define("ExampleCounter", has_children=False)
 
 
 def ExampleCounter(on_count_change, button_text, button_id):
-    return _ExampleComponent(
+    return _js_module.ExampleComponent(
         {
             "onCountChange": on_count_change,
             "buttonText": button_text,
