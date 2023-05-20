b422cd91a34c97ecc5174b4d3f60445c3dee2d58
rmorshea <ryan.morshead@gmail.com>
2023-05-20 14:48:08 -0600

    test suite fixes

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,5 @@
-from contextlib import contextmanager
-from functools import wraps
 from pathlib import Path
 from shutil import rmtree
-from tempfile import TemporaryDirectory
 from typing import Callable
 
 from nox import Session, session
@@ -14,66 +11,25 @@ TEMPLATE_DIR = HERE / "{{cookiecutter.repository_name}}"
 SessionFunc = Callable[[Session], None]
 
 
-def build_test_repo(install: bool = True) -> Callable[[SessionFunc], SessionFunc]:
-    """Build a test repo from test-config.yaml before a session"""
-
-    def decorator(session_func: SessionFunc) -> SessionFunc:
-        @wraps(session_func)
-        def wrapper(session: Session) -> None:
-            _build_test_repo(session, install)
-            return session_func(session)
-
-        return wrapper
-
-    return decorator
-
-
-def install_latest_reactpy(session_func: SessionFunc) -> SessionFunc:
-    # install the latest version of ReactPy by pulling it from the main repo
-
-    @wraps(session_func)
-    def wrapper(session: Session) -> None:
-        try:
-            session.run(
-                "git",
-                "clone",
-                "https://github.com/reactive-python/reactpy.git",
-                external=True,
-            )
-            session.install("./reactpy[testing,starlette]")
-            session_func(session)
-        finally:
-            reactpy_dir = HERE / "reactpy"
-            if reactpy_dir.exists():
-                rmtree(reactpy_dir)
-
-    return wrapper
-
-
-@session
-def test(session: Session) -> None:
-    session.notify("test_suite", posargs=session.posargs)
-    session.notify("test_style")
-
-
-@session
-@build_test_repo()
-@install_latest_reactpy
+@session(tags=["test"])
 def test_suite(session: Session) -> None:
+    build_test_repo(session)
+    install_latest_reactpy(session)
     session.chdir("test-repo")
     session.run("playwright", "install", "chromium")
     session.run("pytest", "tests", "--import-mode=importlib", *session.posargs)
 
 
-@session
-@build_test_repo(install=False)
+@session(tags=["test"])
 def test_style(session: Session) -> None:
+    build_test_repo(session, install=False)
     session.install("black", "flake8")
     session.run("black", "--check", "test-repo", *list(map(str, HERE.glob("*.py"))))
     session.run("flake8", "test-repo")
 
 
-def _build_test_repo(session: Session, install: bool) -> None:
+def build_test_repo(session: Session, install: bool = True) -> None:
+    """Build a test repo from test-config.yaml"""
     # Need to remove node_modules so cookiecutter doesn't think since the cookiecutter
     # will try to format those files if present
     for path in TEMPLATE_DIR.rglob("node_modules"):
@@ -94,3 +50,15 @@ def _build_test_repo(session: Session, install: bool) -> None:
         session.install(".")
         session.install("-r", "requirements.txt")
         session.chdir("..")
+
+
+def install_latest_reactpy(session: Session) -> SessionFunc:
+    # install the latest version of ReactPy by pulling it from the main repo
+    try:
+        session.install(
+            "reactpy[testing,starlette] @ git+https://github.com/reactive-python/reactpy"
+        )
+    finally:
+        reactpy_dir = HERE / "reactpy"
+        if reactpy_dir.exists():
+            rmtree(reactpy_dir)
